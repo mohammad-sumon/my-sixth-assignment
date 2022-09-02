75642d084741831a5ce917b0f86ab9f74dd8d055
mohammad-sumon <sumon3011@gmail.com>
2022-09-03 00:01:04 +0600

    modal added

--- index.js
+++ index.js
@@ -48,27 +48,59 @@ const displayCategoryItems = (data) => {
         const categoryContainer = document.getElementById('category-container');
         const div = document.createElement('div');
         div.innerHTML = `
-        <div class="row g-3 p-3 shadow border my-3">
-            <div class="col-md-4 my-3">
-            <img src="${singleData.thumbnail_url}" class="img-fluid rounded-start" alt="..." />
+        <div class="row g-0 p-3 shadow">
+            <div class="col-md-4">
+                <img src="${singleData.thumbnail_url}" class="img-fluid rounded-start" alt="..." />
             </div>
             <div class="col-md-8">
-            <div class="card-body">
-                <h5 class="card-title">${singleData.title}</h5>
-                <p class="card-text" style="height: 150px; border:1px solid red; overflow: hidden; text-overflow: ellipsis;">${singleData.details}</p>
-                <div class="card-text d-flex mx-5">
-                <div>
-                    <img src="#" alt="" />
-                    <h3>Jane Cooper</h3>
+                <div class="card-body">
+                    <h5 class="card-title">${singleData.title ? singleData.title : 'No Result Found'}</h5>
+                    <p class="card-text" style="height: 150px; border:1px solid red; overflow: hidden; text-overflow: ellipsis;">${singleData.details}</p>
+                    <div class="card-text d-flex justify-content-between">
+                        <div class="d-flex">
+                            <img src="${singleData.author.img}" alt="" style="width: 50px" class="rounded-circle" />
+                            <h3>${singleData.author.name ? singleData.author.name : 'No Result Found'}</h3>
+                        </div>
+                        <div>Total View: ${singleData.total_view ? singleData.total_view : 'No Result Found'}</div>
+                        <div><button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                        data-bs-target="#exampleModal" onclick="loadCategoryDetails('${singleData._id}')">Details</button></div> 
+                    </div>
                 </div>
-                <div>View</div>
-                <div>btn</div>
-                </div>
-            </div>
             </div>
         </div>
         `;
         categoryContainer.appendChild(div);
     })
 }
+
+const loadCategoryDetails = (_id) => {
+    // console.log(_id);
+    const url = `https://openapi.programming-hero.com/api/news/${_id}`;
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayCategoryDetails(data))
+}
+
+const displayCategoryDetails = (data) => {
+    console.log(data);
+    const dataIndex = data.data[0];
+
+    // const modalTitle = document.getElementById('exampleModalLabel');
+    // modalTitle.innerText = dataIndex.author.name;
+
+    const modalBody = document.getElementById('modal-body');
+    modalBody.textContent = '';
+    const modalDiv = document.createElement('div');
+    modalDiv.innerHTML = `
+        <img src="${dataIndex.thumbnail_url}">
+        <p>Title: ${dataIndex.title}</p>
+        <p>Author Name: ${dataIndex.author.name}</p>
+        <p>Total View: ${dataIndex.total_view}</p>
+        <p>Rating: ${dataIndex.rating.number}</p>
+    `;
+    modalBody.appendChild(modalDiv);
+
+}
+
 loadNews();
