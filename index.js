e865d8f657d12e9f207bd0568fbf1d9e85dee76c
mohammad-sumon <sumon3011@gmail.com>
2022-09-02 21:16:09 +0600

    category items added from dynamical data

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('hello from js');
+
+
+const loadNews = () => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayNewsCategory(data))
+}
+
+const displayNewsCategory = (news) => {
+    console.log(news);
+    console.log(news.data);
+    const newsCategory = news.data.news_category;
+    console.log(newsCategory);
+
+    newsCategory.forEach(singleNewsCategory => {
+        const categoryName = singleNewsCategory.category_name;
+        console.log(categoryName);
+
+        const displayCategory = document.getElementById('display-category');
+        const aTag = document.createElement('a');
+        aTag.innerHTML = `
+        <a class="container mx-2" href="#">${categoryName}</a>
+        `;
+        displayCategory.appendChild(aTag);
+    })
+}
+
+
+loadNews();
